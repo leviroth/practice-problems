803f77769601109b3a28014d7b7706f701a2a10e
Levi Roth <levimroth@gmail.com>
2018-03-06 14:37:20 -0500

    Dubious tail-recursive implementation of day 2

--- day2.py
+++ day2.py
@@ -11,17 +11,30 @@ safe_val = partial(safe_get, attr='val', default=0)
 safe_next = partial(safe_get, attr='next', default=None)
 
 
+def reverse(l):
+    prev, cur = None, l
+    while cur is not None:
+        tmp = cur.next
+        cur.next = prev
+        prev, cur = cur, tmp
+    return prev
+
+
 class Solution:
-    def _add_two_numbers(self, l1, l2, carry=0):
+    def _add_two_numbers(self, l1, l2, carry=0, parent=None):
         if l1 is None and l2 is None:
-            return None if carry == 0 else ListNode(carry)
+            if carry == 0:
+                return parent
+            node = ListNode(carry)
+            node.next = parent
+            return node
         else:
             new_n = safe_val(l1) + safe_val(l2) + carry
             digit = new_n % 10
             node = ListNode(digit)
-            node.next = self._add_two_numbers(safe_next(l1), safe_next(l2),
-                                              carry=new_n // 10)
-            return node
+            node.next = parent
+            return self._add_two_numbers(safe_next(l1), safe_next(l2),
+                                         carry=new_n // 10, parent=node)
 
     def addTwoNumbers(self, l1, l2):
         """
@@ -29,4 +42,5 @@ class Solution:
         :type l2: ListNode
         :rtype: ListNode
         """
-        return self._add_two_numbers(l1, l2)
+        result = self._add_two_numbers(l1, l2)
+        return reverse(result)
