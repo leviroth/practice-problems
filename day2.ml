350b9a18a7c8fd31a2a1915c3a0299ada39d02f0
Levi Roth <levimroth@gmail.com>
2018-03-06 14:50:39 -0500

    Make OCaml day 2 more concise
    It is questionable whether this is an improvement. It is arguably less readable.

--- day2.ml
+++ day2.ml
@@ -4,14 +4,14 @@ let add_lists l1 l2 =
   let rec aux l1 l2 carry acc =
     match l1, l2 with
     | [], [] ->
-      let acc = if carry = 0 then acc else carry :: acc in
-      List.rev acc
-    | hd :: tl, [] | [], hd :: tl ->
-      let n = hd + carry in
-      let carry = n / 10 in
-      let digit = n % 10 in
-      aux tl [] carry (digit :: acc)
-    | hd1 :: tl1, hd2 :: tl2 ->
+      List.rev (if carry = 0 then acc else carry :: acc)
+    | hd1 :: tl1, other
+    | other, hd1 :: tl1 ->
+      let hd2, tl2 =
+        match other with
+        | hd :: tl -> hd, tl
+        | [] -> 0, []
+      in
       let n = hd1 + hd2 + carry in
       let carry = n / 10 in
       let digit = n % 10 in
