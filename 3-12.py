304172118f1ee7ce90362ba407d89f6cbf216628
Levi Roth <levimroth@gmail.com>
2018-03-13 14:18:58 -0400

    Remove TreeNode definition from March 12 Python solution

--- 3-12.py
+++ 3-12.py
@@ -1,11 +1,3 @@
-# Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-
 def nums(node, prefix=0):
     if node is None:
         return
